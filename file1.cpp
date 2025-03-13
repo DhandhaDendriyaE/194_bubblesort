dfb9d28fb62882be21e5064bd19980c2ef763442
DhandhaDendriyaE <dandadendriya17@gmail.com>
2025-03-13 11:44:23 +0700

    first commit

---
+++ file1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+int arr[20];
+int n;
+
+void input(){
+    cout << "masukkan banyaknya eleme pada array"
+    cin >> n;
+    if(n<=20){
+        break;
+    }
+    else {
+        cout << "\narray maksimal 20 elemen.\n";
+    }
+}
+cout << endl;
+cout <<"====================" endl;
+cout <<"masukan elemen array" endl;
+cout <<"====================" endl;
+
+for (int i=0;i<n;i++){
+    int pass= 1;
+    do{
+        for (intj=0;j<n-1-pass;j++){
+            if (arr[j])>[1]{
+                int temp;
+                temp = arr[j];
+                arr[j] = 
+            }
+        }
+    } 
+}
