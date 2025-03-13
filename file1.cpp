2f3034c0677165c7321bb6bbac27f119296e9d30
DhandhaDendriyaE <dandadendriya17@gmail.com>
2025-03-13 11:47:23 +0700

    membuat prosedur input

--- file1.cpp
+++ file1.cpp
@@ -26,8 +26,10 @@ for (int i=0;i<n;i++){
             if (arr[j])>[1]{
                 int temp;
                 temp = arr[j];
-                arr[j] = 
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
+        
     } 
 }
